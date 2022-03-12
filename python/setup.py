b28b635705024a944fe0d0f987176c31259814a2
ramo828 <illegalism666@gmail.com>
2022-03-12 11:45:02 +0400

    numb version 2.1.0

--- python/setup.py
+++ python/setup.py
@@ -1,20 +1,8 @@
-from setuptools import setup
+#from setuptools import setup
+from distutils.core import setup
 from Cython.Build import cythonize
 setup(
-    ext_modules = cythonize("pyx/bklib.pyx"),
-    compiler_directives={'language_level' : 3}
-)
-setup(
-    ext_modules = cythonize("pyx/robot.pyx"),
-    compiler_directives={'language_level' : 3}
-)
-
-setup(
-    ext_modules = cythonize("pyx/numb_lib.pyx"),
-    compiler_directives={'language_level' : 3}
-)
-
-setup(
-    ext_modules = cythonize("pyx/statistic.pyx"),
-    compiler_directives={'language_level' : 3}
+    name = 'numb_v2',
+    ext_modules = cythonize(["pyx/*.pyx"]),
+    compiler_directives={'language_level' : 3},
 )
