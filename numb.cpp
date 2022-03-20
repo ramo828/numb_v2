88ca649aa782bd120ed6481525ce0d6dbf7e84db
ramo828 <illegalism666@gmail.com>
2022-03-21 01:40:59 +0400

    numb v2.3.3c fix /work android

--- numb.cpp
+++ numb.cpp
@@ -6,7 +6,10 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	userUI ui;
 	util u;
-	ui.sysConfig("default.dir",u.get_homedir());
+	if(u.getDevice())
+		ui.sysConfig("default.dir",u.get_homedir());
+	else
+		ui.sysConfig("default.dir","/sdcard/work/");
 	run r(argc, argv);
 	r.runner();
 	return 0;
